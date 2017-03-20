e71b69e5926fef1eeef8cbf195f2969b0ed51256
Jonassen Yuan <huddsinyuan2014@gmail.com>
2017-03-20 20:22:43 +0800

    Websocket: Using Websocket to connect with the backends server.

--- SimpleIndex/httpserver.js
+++ SimpleIndex/httpserver.js
@@ -3,21 +3,10 @@
  */
 
 var http = require('http');
-var fs = require('fs');
 
 http.createServer(function (req, res) {
-    var query = require('url').parse(req.url).query;
-    console.log(query);
-
-    var file = require('querystring').parse(query).file;
-
     res.writeHead(200, {'Content-Type' : 'text/plain'});
-    for(var i = 0; i<50; i++) {
-        res.write(i + '\n');
-    }
-
-    var data = fs.readFileSync(file, 'utf8');
-
-    res.write(data);
+    
+    res.write("NGINX: NEXT PAGES");
     res.end();
-}).listen('whatsocket');
+}).listen(4000);
--- SimpleIndex/nodeclass1.js
+++ SimpleIndex/nodeclass1.js
@@ -1,46 +1,15 @@
 var http = require('http');
-var fs = require('fs');
-
-function writeNumbers(res) {
-	var counter = 0;
-	for(var i = 0; i<100; i++) {
-		counter ++ ;
-		res.write(counter.toString()+'\n');
-	}
-}
 
 http.createServer(function(req, res) {
-    var query = require('url').parse(req.url).query;
-    var app = require('querystring').parse(query).file + ".txt";
-
 
     res.writeHead(200, {'Content-Type': 'text/plain'});
 
-    writeNumbers(res);
+    res.write("Welcome to My Blog \r\n");
 
-    setTimeout(function () {
-        console.log('opening ' + app);
-        fs.readFile(app, 'utf8', function(err,data) {
-            if (err) {
-                res.write('Could not find file \n');
-            }
-            else {
-                res.write(data);
-            }
+	res.write("Jonassen 2016\r\n");
 
-            res.end();
-        });
-    },2000);
-}).listen(8124);
+    res.end();
+}).listen(2000);
 
-console.log('Server running at 8124');
-
-process.stdin.resume();
-process.stdin.on('data', function(chunk) {
-    process.stdout.write('data: ' + chunk);
-});
-
-setInterval(function() {
-    console.log('fuck.');
-},1000);
+console.log('Server running at 2000');
 
