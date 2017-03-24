e1ed5fe8ecb9d0b3bc93aab2113bd3e8eea38d7e
Jonassen Yuan <huddsinyuan2014@gmail.com>
2017-03-25 02:00:53 +0800

    WebDesign: Add the registion and the chartjs table. lack of mysql and session support.

--- WebSocket/public/javascripts/cmd.js
+++ WebSocket/public/javascripts/cmd.js
@@ -1,46 +1,279 @@
 /**
  * Created by Jonassen on 2017/3/21.
  */
+$(document).ready(function () {
 
+    $.ajax({
+        type: "GET",
+        url: "/sensordata/sensordata.csv",
+        dataType: "text",
+        success: function (data) {
+            processData(data);
+        }
+    });
 
-$(document).ready(function(){
-    $('#btn_off_gpio').click(()=>{
+    $('#btn_off_gpio1').click(()=> {
         var dataToBack = {
             sensor: 'gpio',
+            number: '1',
             command: 'off'
         };
-        console.log('btn_off_gpio');
-        $.ajax({
-            method: 'POST',
-            data: {data: JSON.stringify(dataToBack)},
-            url: '/cmd',
-            success: function(res){
-                console.log(res);
-            }
-        })
+    console.log('btn_off_gpio---------');
+    $.ajax({
+        type: 'POST',
+        data: {data: JSON.stringify(dataToBack)},
+        url: '/cmd',
+        success: function (res) {
+            console.log(res);
+        }
     })
+})
 
-    $('#btn_on_gpio').click(()=>{
+    $('#btn_on_gpio1').click(()=> {
         var dataToBack = {
             sensor: 'gpio',
+            number: '1',
             command: 'on'
         };
 
-        $.ajax({
-            method: 'POST',
-            data: {data: JSON.stringify(dataToBack)},
-            url: '/cmd',
-            success: function(res){
-                console.log(res);
-            }
-        })
+    console.log('btn_on_gpio------------');
+    $.ajax({
+        type: 'POST',
+        data: {data: JSON.stringify(dataToBack)},
+        url: '/cmd',
+        success: function (res) {
+            console.log(res);
+        }
     })
+})
 
-    $('#aircondition_add').click(()=>{
+    $('#btn_off_gpio2').click(()=> {
+        var dataToBack = {
+            sensor: 'gpio',
+            number: '2',
+            command: 'off'
+        };
+    console.log('btn_off_gpio---------');
+    $.ajax({
+        type: 'POST',
+        data: {data: JSON.stringify(dataToBack)},
+        url: '/cmd',
+        success: function (res) {
+            console.log(res);
+        }
+    })
+})
+
+    $('#btn_on_gpio2').click(()=> {
+        var dataToBack = {
+            sensor: 'gpio',
+            number: '2',
+            command: 'on'
+        };
+
+    console.log('btn_on_gpio------------');
+    $.ajax({
+        type: 'POST',
+        data: {data: JSON.stringify(dataToBack)},
+        url: '/cmd',
+        success: function (res) {
+            console.log(res);
+        }
+    })
+})
+
+    $('#aircondition_add').click(function () {
         console.log('Air condition add button.');
-    })
+    });
 
-    $('#aircondition_minus').click(()=>{
+    $('#aircondition_minus').click(function () {
         console.log('Air condition minus button.');
-    })
+    });
+
+    $('#aircondition_sw').click(function () {
+        console.log('Air condition sw button.');
+    });
+
+    function showChart() {
+        var ctx = document.getElementById("canvas").getContext("2d");
+        window.myLine = Chart.Line(ctx, {
+            data: lineChartData,
+            options: {
+                responsive: true,
+                hoverMode: 'index',
+                stacked: false,
+                title: {
+                    display: true,
+                    text: 'Info'
+                },
+                scales: {
+                    yAxes: [{
+                        type: "linear", // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
+                        display: true,
+                        position: "left",
+                        id: "axis-temperature"
+                    }, {
+                        type: "linear", // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
+                        display: true,
+                        position: "right",
+                        id: "axis-humidity",
+
+                        // grid line settings
+                        gridLines: {
+                            drawOnChartArea: false, // only want the grid lines for one axis to show up
+                        },
+                    }],
+                }
+            }
+        });
+
+        window.myLine.update();
+    }
+
+
+
+    var timeLabels;
+    var humidityDatas;
+    var temperatureDatas;
+
+    var firstsensortimeLabels = [];
+    var firstsensortemperatureDatas = [];
+    var firstsensorhumidityDatas = [];
+
+    var secondsensortimeLabels = [];
+    var secondsensortemperatureDatas = [];
+    var secondsensorhumidityDatas = [];
+
+
+    var lastest_temp = 0;
+    var lastest_humdity = 0;
+    var lastest_led = 0;
+
+    var lineChartData;
+
+    function processData(allText) {
+        var allTextLines = allText.split(/\r\n|\n/);
+        var totalrecord = allTextLines.length;
+        var last100Lines = allTextLines.slice(totalrecord - 96);
+        for (var i = 1; i < last100Lines.length; i++) {
+            var data = last100Lines[i].split(',');
+            if (data.length == 6) {
+                if (data[1] == 'ABCD') {
+                    var b = data[5].slice(13, 16);
+                    var c = data[5].slice(11, 13);
+                    c = (parseInt(c) + 8) % 24;
+                    firstsensortimeLabels.push(c + b);
+                    firstsensortemperatureDatas.push(data[2]);
+                    firstsensorhumidityDatas.push(data[3]);
+                    lastest_temp = data[2];
+                    lastest_humdity = data[3];
+                    lastest_led = data[4];
+                }
+                else if (data[1] == 'WXYZ') {
+                    var b = data[5].slice(13, 16);
+                    var c = data[5].slice(11, 13);
+                    c = (parseInt(c) + 8) % 24;
+                    secondsensortimeLabels.push(c + b);
+                    secondsensortemperatureDatas.push(data[2]);
+                    secondsensorhumidityDatas.push(data[3]);
+                }
+            }
+        }
+        $('#index_temp').html('<h3> ' + lastest_temp +' C </h3>');
+        $('#index_humidity').html('<h3> ' + lastest_humdity + ' %RH</h3>');
+        $('#index_led').html('<h3> ' + lastest_led + ' </h3>');
+
+    }
+
+    $('#GetAData').click(function () {
+        timeLabels = firstsensortimeLabels;
+        humidityDatas = firstsensorhumidityDatas;
+        temperatureDatas = firstsensortemperatureDatas;
+        lineChartData = {
+            labels: timeLabels,
+            datasets: [{
+                label: "Temperature",
+                borderColor: window.chartColors.red,
+                backgroundColor: window.chartColors.red,
+                fill: false,
+                data: temperatureDatas,
+                yAxisID: "axis-temperature",
+            }, {
+                label: "Humidity",
+                borderColor: window.chartColors.blue,
+                backgroundColor: window.chartColors.blue,
+                fill: false,
+                data: humidityDatas, //randomfactor
+                yAxisID: "axis-humidity"
+            }]
+        };
+
+        showChart();
+    });
+
+    $('#GetBData').click(function () {
+        timeLabels = secondsensortimeLabels;
+        humidityDatas = secondsensorhumidityDatas;
+        temperatureDatas = secondsensortemperatureDatas;
+        lineChartData = {
+            labels: timeLabels,
+            datasets: [{
+                label: "Temperature",
+                borderColor: window.chartColors.red,
+                backgroundColor: window.chartColors.red,
+                fill: false,
+                data: temperatureDatas,
+                yAxisID: "axis-temperature",
+            }, {
+                label: "Humidity",
+                borderColor: window.chartColors.blue,
+                backgroundColor: window.chartColors.blue,
+                fill: false,
+                data: humidityDatas, //randomfactor
+                yAxisID: "axis-humidity"
+            }]
+        };
+        showChart();
+    });
+
+    //document.getElementById('GetBData').addEventListener('click', function () {
+    //
+    //});
+
+//    $('#another_login_button').click(()=> {
+//        console.log('click');
+//        var dataToBack = {
+//            username: $('#login_user').value,
+//            password: $('#login_password').value
+//        };
+//    console.log('another_login_button');
+//    $.ajax({
+//        type: 'POST',
+//        data: {data: JSON.stringify(dataToBack)},
+//        url: '/info',
+//        success: function (res) {
+//            console.log(res);
+//        }
+//    })
+//
+//
+//})
+//
+//    $('#login_button').click(()=> {
+//        var dataToBack = {
+//            username: $('#login_user').value,
+//            password: $('#login_password').value
+//        };
+//        console.log('login_button');
+//        $.ajax({
+//            type: 'POST',
+//            data: {data: JSON.stringify(dataToBack)},
+//            url: '/info',
+//            success: function (res) {
+//                console.log(res);
+//            }
+//        })
+//
+//
+//    })
 })
--- WebSocket/routes/index.js
+++ WebSocket/routes/index.js
@@ -4,34 +4,58 @@ var exec = require('child_process').execFile;
 //var spawn = require('child_process').spawn;
 var path = require('path');
 /* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('dashboard');
+var login_status = 0;
+var getusername = function () {
+    var retVal;
+    if(login_status == 1) {
+        retVal = 'wangmeng'
+    }
+    else {
+        retVal = 'Please Log In ...';
+    }
+
+    return retVal;
+};
+
+router.get('/dashboard', function (req, res, next) {
+    res.render('dashboard', {LoginStatus: getusername()});
 });
 
 router.get('/controlpanel', function (req, res, next) {
-    res.render('index', {title: 'Control Panel'});
+    res.render('index', {title: 'Control Panel', LoginStatus: getusername()});
 });
 
 router.get('/test', function (req, res, next) {
-    res.render('layout');
-});
-
-router.get('/test', function (req, res, next) {
-    res.render('layout');
+    res.render('layout',{LoginStatus: getusername()});
 });
 
 router.get('/detail', function (req, res, next) {
-    res.render('detail');
+    res.render('detail', {LoginStatus: getusername()});
 });
 
 
-router.get('/login', function (req, res, next) {
+router.get('/', function (req, res, next) {
     res.render('login');
 });
+
+router.post('/info', function (req, res, next) {
+    var this_username = req.body.username;
+    var this_password = req.body.password;
+
+    if( this_username == 'wangmeng' &&
+        this_password == 'ecnusist') {
+        login_status = 1;
+        res.render('dashboard', {LoginStatus: getusername()});
+    }
+    else
+        res.redirect('/');
+});
+
 
 router.post('/cmd', function (req, res, next) {
     var dataFromFront = JSON.parse(req.body.data);
     console.log(dataFromFront.sensor);
+    console.log(dataFromFront.number);
     console.log(dataFromFront.command);
 
     var dataToFront = {
@@ -39,7 +63,7 @@ router.post('/cmd', function (req, res, next) {
     };
     cmdline = __dirname + '/../public/script/test.sh';
     console.log(cmdline);
-    exec(cmdline, [dataFromFront.sensor, dataFromFront.command], function () {
+    exec(cmdline, [dataFromFront.sensor,dataFromFront.number, dataFromFront.command], function () {
         //promise waiting for call back
         res.json(dataToFront);
     });
